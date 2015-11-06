97f7361dd7e46c7f9fc70682ee526bb45d4dcd3f
Lauri Kortevaara <lauri.kortevaara@gmail.com>
2015-11-06 14:27:24 +0200

    Sample window creation with glut.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
 
-using namespace std;
+void RenderScene(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
 
-int main() {
-    cout << "Hello, World!" << endl;
+void SetupRC(void)
+{
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+}
+
+int main(int argc, char *argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Simple");
+    glutDisplayFunc(RenderScene);
+    SetupRC();
+    glutMainLoop();
     return 0;
 }
