eb372e2e10e1adb6eeb48d011b28f5f0a0f1fe62
Lauri Kortevaara <lauri.kortevaara@gmail.com>
2015-12-12 14:56:33 +0200

    Removed unnecessary drawing code

--- Mesh.cpp
+++ Mesh.cpp
@@ -249,20 +249,7 @@ void Mesh::render() {
     setupModelView();
 
     glBindVertexArray(m_vao);
-    // 1rst attribute buffer : vertices
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-    glVertexAttribPointer(
-    0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-    3,                  // size
-    GL_FLOAT,           // type
-    GL_FALSE,           // normalized?
-    0,                  // stride
-    (void*)0            // array buffer offset
-    );
-    // Draw the triangle !
     glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-    glDisableVertexAttribArray(0);
     //glDrawElements(GL_TRIANGLES, 1, GL_UNSIGNED_BYTE, &m_ibo);
     glBindVertexArray(0);
 }
